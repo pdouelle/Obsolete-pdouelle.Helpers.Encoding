876caf704e118c845fd70169669230a76e9d2bc5
Philippe Douelle <philippe.douelle@consulting-for.edenred.com>
2021-07-29 14:40:49 +0200

    add Sha256 hash

---
+++ pdouelle.Helpers.Encoding/Sha256.cs
@@ -0,0 +1,24 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace pdouelle.Helpers.Encoding
+{
+    public static class Sha256
+    {
+        public static string Hash(string value)
+        {
+            var sb = new StringBuilder();
+
+            using (var hash = SHA256.Create())            
+            {
+                System.Text.Encoding enc = System.Text.Encoding.UTF8;
+                var result = hash.ComputeHash(enc.GetBytes(value));
+
+                foreach (var b in result)
+                    sb.Append(b.ToString("x2"));
+            }
+
+            return sb.ToString();
+        }
+    }
+}
